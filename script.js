9ba8b8e7fd66966c850f86b37ed9204eb7f37168
sergetoure <serge.toure@eviatech.com>
2026-04-05 01:31:27 +0200

    small fix

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const span = document.getElementById('countDisplay')
-
+const follow=document.getElementById('follow')
 
 function newFollower() {
     let counter = 0
-
     function increaseCount() {
         counter++
         console.log('new follower added')
         span.textContent = counter
     }
 
-
     return increaseCount
 }
 
-let increaseCount=newFollower()
+let increaseCounter=newFollower()
+
+follow.addEventListener('click',increaseCounter)
 
 
