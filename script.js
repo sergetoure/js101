a13aac42e266a1a200d4e57a21569897b9bcf288
sergetoure <serge.toure@eviatech.com>
2026-04-05 17:18:58 +0200

    addig checkcounter

--- script.js
+++ script.js
@@ -7,16 +7,6 @@ function newFollower() {
         counter++
         console.log('new follower added')
         span.textContent = counter
-        checkCount()
-    }
-
-    function checkCount(){
-        if (counter===10){
-            alert('congratulation you reach 10 followers')
-
-        } else if(counter===20){
-            alert('Hey you rock , you reach 20 followers')
-        }
     }
 
     return increaseCount
