151d3cbaefe7d638dd89d708819f9657f21dfcef
sergetoure <serge.toure@eviatech.com>
2026-04-06 17:56:36 +0200

    code optimized

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,31 +1,12 @@
 const feedbackform = document.getElementById('productFeedback')
 const userInfoDiv = document.getElementById('userInfo')
-
 feedbackform.addEventListener('submit', displayFeedbackInfo)
-
 function displayFeedbackInfo(e) {
     e.preventDefault()
-
     const feedbackInfo = new FormData(e.target)
-    console.log('form submited succesfully')
-    const userName = document.getElementById('userName')
-    const userAge = document.getElementById('userAge')
-    const userEmail = document.getElementById('userEmail')
-    const userJob = document.getElementById('userJob')
-    const userDesignation = document.getElementById('userDesignation')
-    const userProductChoice = document.getElementById('userProductChoice')
-    const userFeedback = document.getElementById('userFeedback')
-
-    userName.innerText = feedbackInfo.get('userName')
-    userAge.innerText = feedbackInfo.get('userAge')
-    userEmail.innerText = feedbackInfo.get('userEmail')
-    userJob.innerText = feedbackInfo.get('userJob')
-    userDesignation.innerText = feedbackInfo.get('userDesignation')
-    userProductChoice.innerText = feedbackInfo.get('userProductChoice')
-    userFeedback.innerText = feedbackInfo.get('userFeedback')
-
-    userInfoDiv.setAttribute('style', 'display:true')
-
-
-
+    const fields = ['userName', 'userAge', 'userEmail', 'userJob', 'userDesignation', 'userProductChoice', 'userFeedback']
+    fields.forEach(field => {
+        document.getElementById(field).innerText = feedbackInfo.get(field)
+    })
+    userInfoDiv.style.display = 'block'
 }
