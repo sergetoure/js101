6c679ef15496ce7f7a066cf1c75c6049544dfb52
sergetoure <serge.toure@eviatech.com>
2026-04-15 00:06:45 +0200

    work in progress

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -8,8 +8,8 @@ function bookSystem() {
         //push to books list
         books.push(book)
         //construct the book line in table
-        let bookItemHtml = books.map(({ name, author, description, nopages }) =>
-            `<tr><td>${name}</td><td>${author}</td><td>${description}</td><td>${nopages}</td></tr>`
+        let bookItemHtml = books.map((index,{ name, author, description, nopages }) =>
+            `<tr><td>${name}</td><td>${author}</td><td>${description}</td><td>${nopages}</td><td><button onclick=${editBook(index)}>Edit</button></td><td><button ionclick=${deleteBook(index)}>Delete</button></td></tr>`
         ).join('')
 
 
@@ -18,13 +18,39 @@ function bookSystem() {
         booksDiv.innerHTML =
             `<table>
         <thead>
-        <th>name</th><th>author</th><th>description</th><th>nopages</th>
+        <th>name</th><th>author</th><th>description</th><th>nopages</th><th></th><th></th>
         </thead>
         <tbody>${bookItemHtml}</tbody>
         </table>`
 
         //reset form
         booksystemForm.reset()
+
+      function editBook(index){
+
+      }
+
+       function deleteBook(index){
+
+        books.splice(index,1)
+        let bookItemHtml = books.map(({ name, author, description, nopages }) =>
+            `<tr><td>${name}</td><td>${author}</td><td>${description}</td><td>${nopages}</td><td><button onclick=${editBook(index)}>Edit</button></td><td><button ionclick=${deleteBook(index)}>Delete</button></td></tr>`
+        ).join('')
+
+
+        //add to div id=books
+
+        booksDiv.innerHTML =
+            `<table>
+        <thead>
+        <th>name</th><th>author</th><th>description</th><th>nopages</th><th></th><th></th>
+        </thead>
+        <tbody>${bookItemHtml}</tbody>
+        </table>`
+
+        
+      }
+
 
     }
 
