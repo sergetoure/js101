9796723e81c04dd67e79cb26f5085e7ea595ff6e
sergetoure <serge.toure@eviatech.com>
2026-04-15 23:46:50 +0200

    structure

---
+++ speedAnalysis/speed.js
@@ -0,0 +1,14 @@
+function startTest(){
+
+}
+
+function endTest(){
+
+}
+
+const startBtn=document.getElementById('startTest')
+const endBtn=document.getElementById('endTest')
+
+startBtn.addEventListener('click',startTest)
+
+endBtn.addEventListener('click',endTest)
